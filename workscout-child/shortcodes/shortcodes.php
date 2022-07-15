1c06bceb71ac2ad945796610b674ce9defed39f5
Jhed Mendoza <jhed.mendoza@hotmail.com>
2022-07-15 20:55:46 +0800

    Added signup form

--- workscout-child/shortcodes/shortcodes.php
+++ workscout-child/shortcodes/shortcodes.php
@@ -2,17 +2,24 @@
 
 function shortcode_fc_search_job_and_location() {
 	ob_start();
-  get_template_part('template-parts/forms/search-job-and','locations');
+  	get_template_part('template-parts/forms/search-job-and','locations');
 	return ob_get_clean();
 }
 add_shortcode('fc_search_job_and_location', 'shortcode_fc_search_job_and_location');
 
 function shortcode_fc_login_form() {
 	ob_start();
-  get_template_part('template-parts/forms/login','form');
+  	get_template_part('template-parts/forms/login','form');
 	return ob_get_clean();
 }
 add_shortcode('fc_login_form', 'shortcode_fc_login_form');
+
+function shortcode_fc_signup_form() {
+	ob_start();
+  	get_template_part('template-parts/forms/signup','form');
+	return ob_get_clean();
+}
+add_shortcode('fc_signup_form', 'shortcode_fc_signup_form');
 
 
 function shortcode_fc_latest_news($attr) {
@@ -25,3 +32,5 @@ function shortcode_fc_latest_news($attr) {
 	return ob_get_clean();
 }
 add_shortcode('fc_latest_news', 'shortcode_fc_latest_news');
+
+
---
+++ workscout-child/template-parts/forms/signup-form.php
@@ -0,0 +1,24 @@
+<form id="fc-sign-up-form">
+  <div class="row">
+    <div class="col">
+      <label for="signup_first_name">First Name <span class="required">*</span></label>
+      <input type="text" name="signup_first_name" id="signup-first-name" />
+    </div>
+    <div class="col">
+      <label for="signup_last_name">Last Name <span class="required">*</span></label>
+      <input type="text" name="signup_last_name" id="signup-last-name" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+      <label for="signup_email_address">Email Address <span class="required">*</span></label>
+      <input type="text" name="signup_email_address" id="signup-email-address" />
+    </div>
+    <div class="col">
+      <label for="signup_password">Password <span class="required">*</span></label>
+      <input type="password" name="signup_password" id="signup-password" />
+    </div>
+  </div>
+
+</form>
