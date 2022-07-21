f813fe925ae1d0978de0fc37fcbdd8aa81c7cc3a
Jhed Mendoza <jhed.mendoza@hotmail.com>
2022-07-21 22:59:21 +0800

    Additional updates on registration form

--- workscout-child/template-parts/forms/signup-form.php
+++ workscout-child/template-parts/forms/signup-form.php
@@ -2,18 +2,18 @@
   <div class="row">
     <div class="col">
       <label for="signup_first_name">First Name <span class="required">*</span></label>
-      <input type="text" name="signup_first_name" id="signup-first-name" />
+      <input type="text" name="signup_first_name" id="signup-firstname" />
     </div>
     <div class="col">
       <label for="signup_last_name">Last Name <span class="required">*</span></label>
-      <input type="text" name="signup_last_name" id="signup-last-name" />
+      <input type="text" name="signup_last_name" id="signup-lastname" />
     </div>
   </div>
 
   <div class="row">
     <div class="col">
-      <label for="signup_email_address">Email Address <span class="required">*</span></label>
-      <input type="text" name="signup_email_address" id="signup-email-address" />
+      <label for="signup_email">Email Address <span class="required">*</span></label>
+      <input type="text" name="signup_email" id="signup-email" />
     </div>
     <div class="col">
       <label for="signup_username">Username <span class="required">*</span></label>
